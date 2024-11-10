0262a867468c474c77da3c6bd1b58669397fa300
happo31 <happo31@gmail.com>
2024-11-10 07:21:22 +0000

    feat: トークンを識別できるようにする ~P33

--- rustack/src/main.rs
+++ rustack/src/main.rs
@@ -1,27 +1,11 @@
-mod stack;
+use parse::parse;
+mod op;
+mod parse;
 
 fn main() {
-    let mut stack = vec![];
+    for line in std::io::stdin().lines().flatten() {
+        let (stack, _) = parse(&line.split_whitespace().collect::<Vec<_>>());
 
-    for line in std::io::stdin().lines() {
-        if let Ok(line) = line {
-            let words: Vec<_> = line.split(' ').collect();
-
-            for word in words {
-                if let Ok(parsed) = word.parse::<i32>() {
-                    stack.push(parsed);
-                } else {
-                    match word {
-                        "+" => stack::add(&mut stack),
-                        "-" => stack::sub(&mut stack),
-                        "*" => stack::mul(&mut stack),
-                        "/" => stack::div(&mut stack),
-                        _ => panic!("{word:?} could not be parsed."),
-                    }
-                }
-            }
-
-            println!("Stack: {stack:?}");
-        }
+        println!("{stack:?}");
     }
 }
---
+++ rustack/src/parse.rs
@@ -0,0 +1,93 @@
+#[derive(Debug, PartialEq, Eq)]
+pub enum Value<'a> {
+    Num(i32),
+    Op(&'a str),
+    Block(Vec<Value<'a>>),
+}
+
+impl<'src> Value<'src> {
+    pub fn as_num(&self) -> i32 {
+        match self {
+            Value::Num(num) => *num,
+            _ => panic!("Value is not a number"),
+        }
+    }
+}
+
+pub fn op_parse<'src>(word: &'src str) -> Result<Value<'src>, &'src str> {
+    match word {
+        "+" => Ok(Value::Op("+")),
+        "-" => Ok(Value::Op("-")),
+        "*" => Ok(Value::Op("*")),
+        "/" => Ok(Value::Op("/")),
+        _ => Err(word),
+    }
+}
+
+pub fn num_parse<'src>(word: &'src str) -> Result<Value<'src>, ()> {
+    match word.parse::<i32>() {
+        Ok(num) => Ok(Value::Num(num)),
+        Err(_) => Err(()),
+    }
+}
+
+pub fn parse<'src, 'a>(input: &'a [&'src str]) -> (Value<'src>, &'a [&'src str]) {
+    let mut tokens = vec![];
+    let mut words = input;
+
+    while let Some((&word, mut rest)) = words.split_first() {
+        if word.is_empty() {
+            break;
+        }
+
+        match word {
+            "{" => {
+                let value;
+                (value, rest) = parse(rest);
+                tokens.push(value);
+            }
+            "}" => {
+                return (Value::Block(tokens), rest);
+            }
+            _ => {
+                if let Ok(value) = num_parse(word) {
+                    tokens.push(value);
+                } else if let Ok(op) = op_parse(word) {
+                    tokens.push(op);
+                } else {
+                    panic!("{:?} could not be parsed.", word);
+                }
+            }
+        }
+        words = rest;
+    }
+    (Value::Block(tokens), words)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_num_parse() {
+        assert_eq!(num_parse("123"), Ok(Value::Num(123)));
+        assert_eq!(num_parse("abc"), Err(()));
+    }
+
+    #[test]
+    fn test_block_parse() {
+        let input = &["1", "2", "+", "{", "3", "4", "}"];
+        assert_eq!(
+            parse(input),
+            (
+                Value::Block(vec![
+                    Value::Num(1),
+                    Value::Num(2),
+                    Value::Op("+"),
+                    Value::Block(vec![Value::Num(3), Value::Num(4)])
+                ]),
+                &input[0..0]
+            )
+        );
+    }
+}
