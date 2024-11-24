4804f0a39316a91c253509488e5eb5f83ee09b51
happo31 <happo31@gmail.com>
2024-11-24 07:09:16 +0000

    feat: ファイルから実行できるようにする ~P49

--- rustack/src/main.rs
+++ rustack/src/main.rs
@@ -1,3 +1,5 @@
+use std::io::{BufRead, BufReader};
+
 use eval::eval_with_block as eval;
 use parse::parse;
 use vm::Vm;
@@ -7,6 +9,25 @@ mod parse;
 mod vm;
 
 fn main() {
+    if let Some(f) = std::env::args()
+        .nth(1)
+        .and_then(|f| std::fs::File::open(f).ok())
+    {
+        run_batch(BufReader::new(f));
+    } else {
+        repl();
+    }
+}
+
+fn run_batch(buffer: impl BufRead) {
+    let mut vm: Vm = Vm::new();
+    for line in buffer.lines().flatten() {
+        let (codes, _) = parse(line.split_whitespace().collect::<Vec<_>>().as_slice());
+        eval(codes, &mut vm);
+    }
+}
+
+fn repl() {
     let mut vm: Vm = Vm::new();
     for line in std::io::stdin().lines().flatten() {
         let (codes, _) = parse(line.split_whitespace().collect::<Vec<_>>().as_slice());
